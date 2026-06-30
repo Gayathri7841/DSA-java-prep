36070353387068ffe82869bdaef8cb580be0379a
Gayathri7841 <boinagayathri1201@gmail.com>
2026-06-30 19:50:49 +0530

    Time: 61 ms (62.78%), Space: 44.2 MB (46.1%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int beautySum(String s) {
+    
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+                int freq[]=new int[26];
+            for(int j=i;j<s.length();j++){
+int index=s.charAt(j)-'a';
+freq[index]++;
+  int max=Integer.MIN_VALUE;
+            int min=Integer.MAX_VALUE;
+            for(int k=0;k<26;k++){
+if(freq[k]>0){
+    max=Math.max(max,freq[k]);
+    min=Math.min(min,freq[k]);
+}
+            }
+            sum=sum+(max-min);
+            }
+          
+        }
+        return sum;
+    }
+}
