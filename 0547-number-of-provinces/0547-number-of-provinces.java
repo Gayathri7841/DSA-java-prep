54d8ab4b5e9dbf201b217e5aaac059df73a16d81
Gayathri7841 <boinagayathri1201@gmail.com>
2026-07-29 12:41:20 +0530

    Time: 2 ms (42.12%), Space: 47.4 MB (31.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,15 +3,19 @@ class Solution {
         int count=0;
         boolean visited[]=new boolean[isConnected[0].length];
        for(int start=0;start<visited.length;start++){
-        Queue<Integer> q=new LinkedList<>();
+      
         if(!visited[start]){
+              Queue<Integer> q=new ArrayDeque<>();
+            visited[start]=true;
 q.offer(start);
 while(!q.isEmpty()){
     int node=q.poll();
-    visited[node]=true;
+    
     for(int j=0;j<isConnected.length;j++){
         if(isConnected[node][j]==1&&!visited[j]){
+            visited[j]=true;
             q.offer(j);
+
         }
     }
 }
