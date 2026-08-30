46e89309416c3e897179feff78bec3e7c0d6e158
Gayathri7841 <boinagayathri1201@gmail.com>
2026-08-30 15:29:00 +0530

    Time: 206 ms (52.8%), Space: 47.4 MB (20.96%) - LeetHub

---
+++ 4041-minimum-operations-to-form-subset-sum-ii/4041-minimum-operations-to-form-subset-sum-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    class Pair{
+        int number;
+        int operations;
+        Pair(int number,int operations){
+            this.number=number;
+            this.operations=operations;
+        }
+    }
+    public int minOperations(int[] nums, int sum) {
+       int n=nums.length;
+        int dp[]=new int[sum+1];
+        final int INF=Integer.MAX_VALUE;
+        Arrays.fill(dp,INF);
+        dp[0]=0;
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            List<Pair> list=generate(nums[i],sum);
+           
+           
+int clonee[]=dp.clone();
+for(Pair p:list){
+    int sum1=p.number;
+    int opss=p.operations;
+for(int k=0;k<clonee.length;k++){
+    if(clonee[k]!=INF){
+int s=k;
+int o=clonee[k];
+if(s+sum1<clonee.length){
+dp[s+sum1]=Math.min(dp[s+sum1],clonee[k]+opss);
+}
+    }
+}
+}
+
+        }
+       if(dp[sum]==INF) return -1;
+        return dp[sum]; 
+    }
+    private List<Pair> generate(int num,int sum){
+        Map<Integer,Integer> map=new HashMap<>();
+        Queue<Integer> q=new LinkedList<>();
+        List<Pair> list=new ArrayList<>();
+        q.add(num);
+        map.put(num,0);
+        while(!q.isEmpty()){
+            int n=q.poll();
+            int o=map.get(n);
+            if(n<=sum) list.add(new Pair(n,o));
+            int mul=n*2;
+            if(mul<=sum&&!map.containsKey(mul)){
+                map.put(mul,o+1);
+                q.add(mul);
+            }
+            int div=n/2;
+            if(div>=1&&!map.containsKey(div)){
+                map.put(div,o+1);
+                q.add(div);
+            }
+        }
+        return list;
+    }
+}
